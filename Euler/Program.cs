c9156d8cac538bfb8c854095a5a7de322dbf9992
Uonai <cs.mccaleb@gmail.com>
2020-08-24 21:51:25 -0500

    Add project files.

---
+++ Euler/Program.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+
+            for (var i = 0; i < 5; i++)
+            {
+                number++;
+                var url = $"https://projecteuler.net/problem={number}";
+                var web = new HtmlWeb();
+                var doc = web.Load(url);
+                string title = doc.DocumentNode.SelectSingleNode("//h2").InnerText;
+                Console.WriteLine("PROBLEM TITLE:");
+                Console.WriteLine(title);
+                Console.WriteLine("PROBLEM CONTENT:");
+                var problemLines = doc.DocumentNode.SelectNodes("//div[contains(@class, 'problem_content')]/p");
+                foreach (HtmlNode problemLine in problemLines)
+                {
+                    Console.WriteLine(problemLine.InnerHtml);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
