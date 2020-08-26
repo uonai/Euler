ec31aed468df3785180d7cc167a41284241d877e
Uonai <cs.mccaleb@gmail.com>
2020-08-25 22:27:28 -0500

    Export json file, create Problem model

---
+++ Euler/Models/Problem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Euler.Models
+{
+    public class Problem
+    {
+        public string title { get; set; }
+        public Stack content { get; set; }
+    }
+}
--- Euler/Program.cs
+++ Euler/Program.cs
@@ -1,5 +1,10 @@
-﻿using HtmlAgilityPack;
+﻿using Euler.Models;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Euler
 {
@@ -7,24 +12,48 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Beginning scrape of Project Euler");
             var number = 1;
 
-            for (var i = 0; i < 5; i++)
+            List<Problem> problemList = new List<Problem>();
+
+            // There are 713 problems on project Euler. This loops through every problem and creates a JSON file wi
+            for (var i = 0; i < 714; i++)
             {
                 number++;
                 var url = $"https://projecteuler.net/problem={number}";
                 var web = new HtmlWeb();
                 var doc = web.Load(url);
+
                 string title = doc.DocumentNode.SelectSingleNode("//h2").InnerText;
-                Console.WriteLine("PROBLEM TITLE:");
-                Console.WriteLine(title);
-                Console.WriteLine("PROBLEM CONTENT:");
+
                 var problemLines = doc.DocumentNode.SelectNodes("//div[contains(@class, 'problem_content')]/p");
-                foreach (HtmlNode problemLine in problemLines)
+                var problemLinesFormatted = new Stack();
+                if (problemLines is object)
                 {
-                    Console.WriteLine(problemLine.InnerHtml);
+                    foreach (HtmlNode problemLine in problemLines)
+                    {
+                        problemLinesFormatted.Push(problemLine.InnerHtml);
+                    }
+
+                    problemList.Add(new Problem
+                    {
+                        title = title,
+                        content = problemLinesFormatted
+                    });
+                    Console.WriteLine("Problem added: " + number);
                 }
+             
+             
             }
+
+            string json = JsonConvert.SerializeObject(problemList);
+            
+            // Note: this writes to the /bin/Debug folder of the Project.
+            string path = Directory.GetCurrentDirectory();
+            File.WriteAllText(@path + "ProblemExport.json", json);
+
             Console.ReadLine();
         }
     }
